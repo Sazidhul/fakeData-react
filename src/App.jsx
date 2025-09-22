ed5b1c5835032de4a1fee73ef6a656724aac9486
Sazidhul <sazisazidhul@gmail.com>
2025-09-22 12:37:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+        <div className='main-container'>
+         <div className="cards-container">
+           <div>
+            <img src="" alt="" />
+           </div>
+
+           <div className="card">
+            
+           </div>
+         </div>
+         <div className="cards-container">
+            <h1>This is cart</h1>
+         </div>
+        </div>
+    </>
+  )
+}
+
+export default App
